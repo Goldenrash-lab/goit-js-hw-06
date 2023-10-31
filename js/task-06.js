345a4649289d7d9f9f86df47b2e22f842d00cdfb
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-10-31 16:50:17 +0200

    task6

---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+const inputElem = document.querySelector("#validation-input");
+
+function onInputElemChange(event) {
+  const minLength = +inputElem.dataset.length;
+
+  if (event.target.value.length >= minLength) {
+    event.target.classList.add("valid");
+  } else {
+    event.target.classList.add("invalid");
+  }
+}
+
+function onInputElemFocus(event) {
+  event.target.classList.remove("invalid");
+  event.target.classList.remove("valid");
+}
+
+inputElem.addEventListener("blur", onInputElemChange);
+inputElem.addEventListener("focus", onInputElemFocus);
