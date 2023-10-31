b52a5fa6432c3971cd1cf6dcbac6edf2b417c1f3
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-10-31 15:24:38 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,26 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListElem = document.querySelector("#ingredients");
+
+function ingredientCreate(ingredient) {
+  const liElem = document.createElement("li");
+  liElem.classList.add("item");
+  liElem.textContent = `${ingredient}`;
+  return liElem;
+}
+function ingredientsCreate(ingredients) {
+  return ingredients.map(ingredientCreate);
+}
+
+function renderIngredint(ingredients) {
+  const ingredientsList = ingredientsCreate(ingredients);
+  ingredientsListElem.append(...ingredientsList);
+}
+renderIngredint(ingredients);
