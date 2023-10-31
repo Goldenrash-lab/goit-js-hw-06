b5852ae3f8e7f2c09a7c7ae97dcea487100360e3
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-10-31 17:41:32 +0200

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const placeForDivs = document.querySelector("#boxes");
+const addBtnElem = document.querySelector("[data-create]");
+const delBtnElem = document.querySelector("[data-destroy]");
+
+function createBoxes(amount) {
+  let divWidth = 30;
+  let divHeight = 30;
+  for (let i = 0; i <= amount; i++) {
+    const divElem = document.createElement("div");
+    divElem.style.width = `${(divWidth += 10)}px`;
+    divElem.style.height = `${(divHeight += 10)}px`;
+    divElem.style.backgroundColor = `${getRandomHexColor()}`;
+    placeForDivs.append(divElem);
+  }
+}
+function destroyBoxes() {}
+
+function onAddBtnElemClick(event) {
+  const numberInput = +addBtnElem.previousElementSibling.value;
+  createBoxes(numberInput);
+}
+function onDelBtnElemClick(event) {
+  addBtnElem.previousElementSibling.value = 0;
+  placeForDivs.innerHTML = "";
+}
+
+delBtnElem.addEventListener("click", onDelBtnElemClick);
+addBtnElem.addEventListener("click", onAddBtnElemClick);
