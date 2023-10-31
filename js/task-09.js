b6c59e8a4fdc251b27cc79d367c8163c4633cffa
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-10-31 17:06:51 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnElem = document.querySelector(".change-color");
+
+btnElem.addEventListener("click", onBtnElemClick);
+
+function onBtnElemClick(event) {
+  const randColor = getRandomHexColor();
+  document.body.style.backgroundColor = randColor;
+  btnElem.previousElementSibling.firstElementChild.textContent = randColor;
+}
