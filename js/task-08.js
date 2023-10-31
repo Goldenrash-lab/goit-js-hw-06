8758452cac59b9e7547d8df3be1c7e415d3ac1fa
Yehor Hryhortsev <egor.goldenrash@gmail.com>
2023-10-31 17:01:31 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const formElem = document.querySelector(".login-form");
 
+formElem.addEventListener("submit", onFormElemSubmit);
+
+function onFormElemSubmit(event) {
+  event.preventDefault();
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+  if (email === "" || password === "") {
+    alert("Все поля должны быть заполнены!");
+  } else {
+    console.log({ email, password });
+  }
+}
